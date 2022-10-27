1531cfd73de2c108289f949bda30f4f4d889a8f6
oli <oli@googlemail.com>
2022-10-27 20:39:42 +0100

    Initial Commit

---
+++ mod5Challenge/mod5ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mod5ChallengeApp.swift
+//  mod5Challenge
+//
+//  Created by Oliver on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct mod5ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
